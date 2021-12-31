4335563bc5142b313e1583205f43d892716b8ea7
Antonio <atcarter005@gmail.com>
2021-12-31 01:34:29 -0600

    add private methods to contents controller to be used for the remaining routes

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,4 +3,14 @@ class ContentsController < ApplicationController
     contents = Content.all
     render json: contents
   end
+
+  private 
+
+  def the_content_exists?
+    params[:id] && Content.exists?(params[:id])
+  end
+
+  def content_params
+    params.require(:content).permit(:title, :year, :content_type, :details)
+  end
 end
