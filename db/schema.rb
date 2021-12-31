cf4f7f702f7ab45fcccb78d1e360f5915cff96e5
Antonio <atcarter005@gmail.com>
2021-12-30 23:57:24 -0600

    rollback db to add a type field to content

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_052132) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "contents", force: :cascade do |t|
-    t.string "title"
-    t.integer "year"
-    t.text "details"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "username"
-    t.text "description"
-    t.integer "rating"
-    t.integer "content_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["content_id"], name: "index_reviews_on_content_id"
-  end
-
-  add_foreign_key "reviews", "contents"
 end
