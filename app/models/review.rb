4748b9f006cac841a273405d38333b116b7d18a6
Antonio <atcarter005@gmail.com>
2021-12-31 00:08:51 -0600

    add presence validations to review username and description

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :content
 
-  validates :username
+  validates :username, presence: true
   validates :rating, numericality: { less_than: 6, greater_than: 0 }
-  validates :description
+  validates :description, presence: true
 end
