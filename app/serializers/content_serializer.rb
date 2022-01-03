7b79a3ba6b4b794b8a84a94ac546637f1db49cc1
Antonio <atcarter005@gmail.com>
2022-01-03 07:28:10 -0600

    undo assoctions from serializers due to normalizing redux store on frontend

--- app/serializers/content_serializer.rb
+++ app/serializers/content_serializer.rb
@@ -1,5 +1,4 @@
 class ContentSerializer < ActiveModel::Serializer
   attributes :id, :title, :content_type, :year, :details
 
-  has_many :reviews
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :username, :rating, :description, :content_id
 
-  belongs_to :content
 end
